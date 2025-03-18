5dd93814b3a155d8671eaf25254035be1424132a
Jenny Choi <jchoi766@usc.edu>
2025-03-18 21:17:46 +0000

    finished tree traversals

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,22 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int countHeight(Node *curr);
 
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+  // Add your code below
+	if (root == nullptr) return true;
+	return countHeight(root->left) == countHeight(root->right);	
+
+}
+
+
+int countHeight(Node *curr) {
+	if (curr == nullptr) return 0;
+	if (curr->right == nullptr && curr->left == nullptr) return 0;
+	return 1+countHeight(curr->right)+countHeight(curr->left);
 
 }
 
